bc2b844a5c55863428d971a9b59e41b00d33173b
julia-lundbergish <julialundberg@hotmail.se>
2023-03-11 15:55:42 +0100

    first commit

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Prod router!');
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('test prod router');
+});
+
+module.exports = router;
